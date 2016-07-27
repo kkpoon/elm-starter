eed86f8f5302e2a71c6742d476a977af177bf338
K.K. POON <noopkk@gmail.com>
2016-07-27 23:02:21 +0800

    implement login with server

--- src/Main.elm
+++ src/Main.elm
@@ -1,57 +1,15 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, input, button, br, text)
 import Html.App
-
-
--- MODEL
-
-
-type alias Model =
-  String
-
-
-init : ( Model, Cmd Msg )
-init =
-  ( "Hello", Cmd.none )
-
-
-
--- MESSAGES
-
-
-type Msg
-  = NoOp
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ text model ]
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-  case msg of
-    NoOp ->
-      ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Http
+import Http exposing (defaultSettings)
+import Json.Decode as Decode exposing ((:=))
+import Task
+import Base64
+import String exposing (concat)
 
 
 
@@ -66,3 +24,112 @@ main =
     , update = update
     , subscriptions = subscriptions
     }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { username : String
+  , password : String
+  }
+
+
+init : ( Model, Cmd Msg )
+init =
+  ( Model "" "", Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+  = UpdateUsername String
+  | UpdatePassword String
+  | Login
+  | LoginSuccess LoginResult
+  | LoginFail Http.RawError
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    UpdateUsername username ->
+      ( { model | username = username }, Cmd.none )
+
+    UpdatePassword password ->
+      ( { model | password = password }, Cmd.none )
+
+    Login ->
+      ( model, login)
+
+    LoginSuccess result ->
+      ( model, Cmd.none )
+
+    LoginFail err ->
+      ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type' "text", placeholder "Username", onInput UpdateUsername ] []
+    , br [] []
+    , input [ type' "password", placeholder "Password", onInput UpdatePassword ] []
+    , br [] []
+    , button [ onClick Login ] [ text "Login" ]
+    ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- COMMANDS
+
+
+login : Cmd Msg
+login =
+  Http.send defaultSettings
+    { verb = "POST"
+    , headers = [("Authorization", encodeBasicAuth (concat ["", ":", ""]))]
+    , url = "/login"
+    , body = Http.empty
+    }
+    |> Task.perform LoginFail LoginSuccess
+
+type alias LoginResult =
+  { username : String
+  , token : String
+  }
+
+loginResultDecoder : Decode.Decoder LoginResult
+loginResultDecoder =
+  Decode.object2 LoginResult
+    ("username" := Decode.string)
+    ("token" := Decode.string)
+
+
+encodeBasicAuth : String -> String
+encodeBasicAuth password =
+  case Base64.encode password of
+    Ok value ->
+      value
+    Err error ->
+      ""
