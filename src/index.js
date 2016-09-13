b292eeaf99f3ae53fcf6e12b7d544acf6bb752ea
kkpoon <noopkk@gmail.com>
2016-09-13 14:50:00 +0800

    use fullscreen instead of embed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 require('./index.html');
 
 var Elm = require('./Main.elm');
-var mountNode = document.getElementById('main');
 
 // The third value on embed are the initial values for incomming ports into Elm
-var app = Elm.Main.embed(mountNode);
+var app = Elm.Main.fullscreen();
